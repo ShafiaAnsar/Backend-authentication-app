d46cfafdab540b1ae3a184ad7202d3e70cd58bd3
shafiaansar <shafiaansar142@gmail.com>
2023-09-29 21:50:45 +0500

    User Model

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 import compression from "compression"
 import cors from "cors"
-
+import mongoose, { mongo } from "mongoose"
 const app = express()
  app.use(cors({
     credentials:true
@@ -16,4 +16,10 @@ const app = express()
  const server = http.createServer(app)
  app.listen(8000,()=>{
     console.log("Server is running on http://localhost:8000")
+ })
+ const MongoURL = "mongodb+srv://shafiaansar:athentication@cluster0.rjesorw.mongodb.net/?retryWrites=true&w=majority"
+ mongoose.Promise=Promise
+ mongoose.connect(MongoURL)
+ mongoose.connection.on('error',(error:Error)=>{
+    console.log(error)
  })
