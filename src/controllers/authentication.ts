64f7ee5cf83df00edf4b60a2c023108416fdb823
shafiaansar <shafiaansar142@gmail.com>
2023-09-30 18:12:00 +0500

    Login and register routes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,30 +1,60 @@
 import express from 'express'
 import { createUser, getUserByEmail } from '../db/users'
 import { authentication, random } from '../helpers/index'
-export const register = async(req:express.Request,res:express.Response)=>{
-    try {
-        const {email,password,username} = req.body
-    if(!email || !password || !username){
-        return res.sendStatus(400)
+export const login = async (req:express.Request,res:express.Response)=>{
+ try {
+    const {email,password} = req.body
+    if(!email || !password){
+        res.sendStatus(400).json({message:"All fields required"})
     }
-    const existingUser = await getUserByEmail(email)
-    if(existingUser){
-        return res.sendStatus(400).json('User already exist')
-    }
-    const salt = random()
-    const user = createUser({
-        email,
-        username,
-        authentication:{
-            salt,
-            password:authentication(salt,password)
-        }
-    })
-    return res.status(200).json(user).end()
-    } catch (error) {
-        console.log(error)
-        return res.sendStatus(400)
-    }
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+     if(!user){
+        return res.sendStatus(400).json({message:"User does not exist "})
+     }
+     const expectedHash = authentication(user.authentication.salt,password)
+     if (user.authentication.password != expectedHash){
+        return res.sendStatus(403).json({message:"Wrong Password"})
+     }
+     const salt = random()
+      user.authentication.sessionToken = authentication(salt, user._id.toString())
+    await user.save()
+    res.cookie('AUTH',user.authentication.sessionToken,{domain:'localhost',path:"/"})
+    res.status(200).json(user).end()
+ } catch (error) {
+    console.log('Error',error)
+    res.sendStatus(400)
+ }
 }
+
+
+export const register = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password, username } = req.body;
+        console.log("Request body:",req.body)
+        if (!email || !password || !username) {
+            return res.sendStatus(400);
+        }
+        const existingUser = await getUserByEmail(email);
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const salt = random();
+        const user = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authentication(salt, password),
+            },
+        });
+        // Send the user data as JSON in the response
+        console.log('User:',user)
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500); // Use 500 for internal server error
+    }
+};
+
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import express from "express"
-import http from "http"
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import compression from "compression"
-import cors from "cors"
-import mongoose, { mongo } from "mongoose"
-import router from "./router"
-const app = express()
- app.use(cors({
-    credentials:true
- }))
- app.use(compression())
- app.use(cookieParser())
- app.use(bodyParser.json())
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
 
- const server = http.createServer(app)
- app.listen(8080,()=>{
-    console.log("Server is running on http://localhost:8080")
- })
- const MongoURL = "mongodb+srv://shafiaansar:athentication@cluster0.rjesorw.mongodb.net/?retryWrites=true&w=majority"
- mongoose.Promise=Promise
- mongoose.connect(MongoURL)
- mongoose.connection.on('error',(error:Error)=>{
-    console.log(error)
- })
- 
+import router from './router';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_URL = 'mongodb+srv://shafiaansar:athentication@cluster0.rjesorw.mongodb.net/?retryWrites=true&w=majority'; // DB URI
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
