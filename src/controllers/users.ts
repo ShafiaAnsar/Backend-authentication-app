72dd62eadd7899a1801f236c0a871f9cdd36b31f
shafiaansar <shafiaansar142@gmail.com>
2023-09-30 21:52:45 +0500

    delete and update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -17,14 +17,24 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
   try {
     const { id } = req.params;
 
+    // Attempt to delete the user by their ID
     const deletedUser = await deleteUserById(id);
 
-    return res.json(deletedUser);
+    // Check if the user was not found and handle it appropriately
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Send a success response with the deleted user data
+    return res.status(200).json({ message: 'User deleted successfully', deletedUser });
   } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
+    console.error(error);
+
+    // Send an error response with a meaningful error message
+    return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
+
 export const updateUser = async (req: express.Request, res: express.Response) => {
   try {
     const { id } = req.params;
